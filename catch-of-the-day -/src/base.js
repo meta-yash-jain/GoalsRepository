6b2673757bf98d6ef34d341316208e7e8a7693d1
yash-jain <yash.jain@metacube.com>
2018-06-17 18:01:58 +0530

    Add files via upload

---
+++ catch-of-the-day -/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyA8WGjXAwtjEX8Nt8XBnrMlCxQZvEf6c7g",
+    authDomain: "catch-of-the-day-yash-jain.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-yash-jain.firebaseio.com"
+});
+
+export default base;
