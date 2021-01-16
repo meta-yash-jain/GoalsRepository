1a8ef181be0168301d68fcdfd617caf2147ee32b
Yash Jain <yash.jain@metacube.com>
2021-01-16 18:13:29 +0530

    Added dummy files for rebase

---
+++ file1.js
@@ -0,0 +1,7 @@
+function file1name1(params) {
+    console.log('name1');
+}
+
+function file1name2(params) {
+    console.log('name2');    
+}
---
+++ file2.js
@@ -0,0 +1,7 @@
+function file2name1(params) {
+    console.log('name1');
+}
+
+function file2name2(params) {
+    console.log('name2');    
+}
