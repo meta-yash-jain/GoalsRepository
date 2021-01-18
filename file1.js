d28d7760e3d3bb629bb54ade5236543475bd7f56
Yash Jain <yash.jain@metacube.com>
2021-01-18 11:15:20 +0530

    if else commit

--- file1.js
+++ file1.js
@@ -1,5 +1,10 @@
 function file1name1(params) {
     console.log('name1');
+    if (condition) {
+        console.log('name2');
+    } else {
+        console.log('name3');
+    }
 }
 
 function file1name2(params) {
